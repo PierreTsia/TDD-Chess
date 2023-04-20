ce4d17ceff0560c27c6410a9b8c5c963a3456c82
Pierre_Tsia <pierre.tsiakkaros@gmail.com>
2023-04-20 21:24:58 +0200

    🐛 fix(playground): wrap ChessBoard, ScoreBoard and ControlPanel components in client-only tag to fix server-side rendering issue

--- src/pages/playground/index.vue
+++ src/pages/playground/index.vue
@@ -4,11 +4,13 @@
   <div class="mt-10">
     <div
       class="flex flex-col lg:flex-row w-full justify-center items-center !lg:items-start gap-8">
-      <ChessBoard data-test-id="chessboard" />
-      <div flex flex-col gap-3>
-        <ScoreBoard />
-        <ControlPanel />
-      </div>
+      <client-only>
+        <ChessBoard data-test-id="chessboard" />
+        <div flex flex-col gap-3>
+          <ScoreBoard />
+          <ControlPanel />
+        </div>
+      </client-only>
     </div>
   </div>
 </template>
