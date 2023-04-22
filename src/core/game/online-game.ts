baef8e79901e16ae24ae709774c674eefc227647
Pierre_Tsia <pierre.tsiakkaros@gmail.com>
2023-04-22 15:40:51 +0200

    🔨 refactor(online-game.ts): extract generateGameStateUpdate method to generate game state update object ✨ feat(online-game.ts): add support for persisting game state updates to Supabase database

--- src/core/game/online-game.ts
+++ src/core/game/online-game.ts
@@ -1,5 +1,6 @@
 import { Game } from '~/core/game/game'
 import type { IMove, IOnlineGame, IPlayer } from '~/core/types'
+import type { GameStateUpdate } from '~/modules/types/supabase'
 import type { MultiplayerService } from '~/services/api'
 
 export class MultiplayerGameEngine extends Game implements IOnlineGame {
@@ -17,44 +18,37 @@ export class MultiplayerGameEngine extends Game implements IOnlineGame {
 
   startGame() {
     super.startGame()
-    this.apiService
-      .persistMove(
-        this.gameId!,
-        {
-          board: JSON.stringify(this.board),
-          current_player_id: this.players[0].id,
-        },
-        this.status,
-        null
-      )
-      .then(() => {
-        // eslint-disable-next-line no-console
-        console.log('game started')
-      })
+    this.updateOnlineGame()
   }
 
   makeMove(move: IMove): boolean {
     if (super.makeMove(move)) {
-      const nextPlayer =
-        move.piece.color === 'white' ? this.players[1] : this.players[0]
-      this.apiService
-        .persistMove(
-          this.gameId!,
-          {
-            board: JSON.stringify(this.board),
-            current_player_id: nextPlayer.id,
-            captured_pieces: JSON.stringify(this.capturedPieces),
-            move_history: JSON.stringify(this.moveHistory),
-          },
-          this.status,
-          this.gameWinner?.id ?? null
-        )
-        .then((r) => {
-          // eslint-disable-next-line no-console
-          console.log('move persisted', r)
-        })
+      this.updateOnlineGame()
       return true
     }
     return false
+  }
+
+  private updateOnlineGame() {
+    this.apiService
+      .persistMove(
+        this.gameId!,
+        this.generateGameStateUpdate(),
+        this.status,
+        this.gameWinner?.id ?? null
+      )
+      .then(() => {
+        // eslint-disable-next-line no-console
+        console.log('game state persisted')
+      })
+  }
+
+  private generateGameStateUpdate(): GameStateUpdate {
+    return {
+      board: JSON.stringify(this.board),
+      current_player_id: this.currentPlayer.id,
+      captured_pieces: JSON.stringify(this.capturedPieces),
+      move_history: JSON.stringify(this.moveHistory),
+    }
   }
 }
