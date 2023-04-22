3af944446250cb1f32bbbf021c17dd131cc35288
Pierre_Tsia <pierre.tsiakkaros@gmail.com>
2023-04-22 11:28:41 +0200

    🚨 test(homePage.spec.ts): remove test case for navigating to multiplayer page ✨ feat(homePage.spec.ts): add test case for navigating to playground page

--- cypress/e2e/homePage.spec.ts
+++ cypress/e2e/homePage.spec.ts
@@ -3,16 +3,15 @@ context('|-> Home Page', () => {
     cy.visit('/')
   })
 
-  it('should navigate to playground and multiplayer page', () => {
+  it('should navigate to playground  page', () => {
     cy.url().should('eq', 'http://localhost:3333/')
     cy.get('[data-test-id="playground-page-btn"]').should('exist').click()
     cy.url().should('eq', 'http://localhost:3333/playground')
+  })
 
-    // go back with the browser back button
-    cy.go('back')
+  it('should navigate to multiplayer page', () => {
     cy.url().should('eq', 'http://localhost:3333/')
     cy.get('[data-test-id="multiplayer-page-btn"]').should('exist').click()
     cy.url().should('eq', 'http://localhost:3333/multi')
   })
-
 })
