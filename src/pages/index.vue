57b105f01cdcf431c35c68ddc950ff53686bb26a
Pierre_Tsia <pierre.tsiakkaros@gmail.com>
2023-04-23 23:19:29 +0200

    🔥 chore(index.vue): remove unused imports and variables

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { useUserStore } from '~/stores/user'
 const router = useRouter()
 
 defineOptions({
   name: 'IndexPage',
 })
-
-const userStore = useUserStore()
-
-const { user } = storeToRefs(userStore)
 
 const multiplayerContent =
   'Create a game and invite your friends to play with you. You can also join a game created by your friends.'
