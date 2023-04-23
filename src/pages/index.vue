101df748ab17aef8055fc4a9a950a30b9a27f1e5
Pierre_Tsia <pierre.tsiakkaros@gmail.com>
2023-04-23 21:06:13 +0200

    💄 feat(home-page): make it a bit nicer

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,28 +10,52 @@ defineOptions({
 const userStore = useUserStore()
 
 const { user } = storeToRefs(userStore)
+
+const multiplayerContent =
+  'Create a game and invite your friends to play with you. You can also join a game created by your friends.'
+
+const playgroundContent =
+  'Playground free mode to test chess engine. If you spot any illegal moves, please report it on GitHub.'
 </script>
 
 <template>
-  <div>
-    <div text-4xl flex justify-center items-center my-4>
-      <o-icon name="i-fluent:chess-20-filled" class="!text-teal-400 mx-2" />
-      <o-text size="xl" mx-2>Welcome {{ user?.username }}</o-text>
-      <o-icon name="i-fluent:chess-20-filled" class="!text-teal-400 mx-2" />
-    </div>
-    <div class="w-full flex mt-6 justify-center gap-4">
-      <o-button
-        data-test-id="multiplayer-page-btn"
-        link
-        @click="router.push('/multi')">
-        Play Online
-      </o-button>
-      <o-button
-        data-test-id="playground-page-btn"
-        link
-        @click="router.push('/playground')">
-        Practice
-      </o-button>
+  <div class="flex h-[calc(100vh-55px)] flex-col justify-center items-center">
+    <h1
+      class="mb-12 text-4xl font-extrabold leading-none tracking-tight !text-teal-400 md:text-5xl lg:text-6xl dark:text-white">
+      TDD Chess Game
+    </h1>
+    <div class="w-full flex justify-center gap-x-8">
+      <o-card
+        divider
+        cover="public/playground.svg"
+        title="Playground mode"
+        :content="playgroundContent"
+        class="!max-w-[300px] divider o-card-hoverable">
+        <template #actions>
+          <o-button
+            data-test-id="playground-page-btn"
+            link
+            @click="router.push('/playground')">
+            Practice
+          </o-button>
+        </template>
+      </o-card>
+      <o-card
+        divider
+        hoverable
+        cover="public/multiplayer.svg"
+        title="Multiplayer mode"
+        :content="multiplayerContent"
+        class="!max-w-[300px] o-card-hoverable">
+        <template #actions>
+          <o-button
+            data-test-id="multiplayer-page-btn"
+            link
+            @click="router.push('/multi')">
+            Play Online
+          </o-button>
+        </template>
+      </o-card>
     </div>
   </div>
 </template>
